b622dc32f4846dde130c27f33b7f1cf50551a70d
belbazanas <belbazanas@gmail.com>
2023-01-30 11:04:43 +0100

    change version'

--- hardhat.config.js
+++ hardhat.config.js
@@ -59,7 +59,7 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.8.1",
+                version: "0.8.7",
                 settings: {
                     optimizer: {
                         enabled: true,
